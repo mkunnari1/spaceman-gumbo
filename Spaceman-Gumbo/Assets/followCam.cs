d48c2f88ca68781eb2110e8eade1375e9d211869
mkunnari1 <mkunnari1@gmail.com>
2020-05-22 18:24:57 -0700

    add boundaries

---
+++ Spaceman-Gumbo/Assets/followCam.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class followCam : MonoBehaviour
+{
+    public Transform target;
+    [SerializeField] float smoothSpeed = 10f;
+    [SerializeField] Vector3 offset;
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
