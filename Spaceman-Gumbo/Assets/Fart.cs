d6c08b4a145c745beb5d388dc28919f24cb4443e
mkunnari1 <mkunnari1@gmail.com>
2020-05-26 17:05:18 -0700

    add partical effects

--- Spaceman-Gumbo/Assets/Fart.cs
+++ Spaceman-Gumbo/Assets/Fart.cs
@@ -6,6 +6,11 @@ public class Fart : MonoBehaviour
     Rigidbody rigidBody;
     [SerializeField] float RotationSpeed = 200.0f;
     [SerializeField] float ThrustPower = 100000.0f;
+    [SerializeField] AudioClip Farts;
+    [SerializeField] AudioClip DeathSound;
+    [SerializeField] ParticleSystem Gas;
+    [SerializeField] ParticleSystem DeathParticle;
+    [SerializeField] ParticleSystem SuccessParticle;
     AudioSource audioSource;
     bool playFart;
     bool toggleFartSound;
@@ -35,6 +40,7 @@ public class Fart : MonoBehaviour
         Thrust();
         Rotation();
         PlayGumboFart();
+        
 
         
     }
@@ -46,6 +52,7 @@ public class Fart : MonoBehaviour
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
                 rigidBody.AddRelativeForce(Vector3.up * ThrustPower * Time.deltaTime);
+                
 
 
             }
@@ -73,28 +80,45 @@ public class Fart : MonoBehaviour
        
     }
 
+    private void DeathAudio()
+    {
+        if ( state == State.Dying)
+        {
+            audioSource.Stop();
+            audioSource.PlayOneShot(DeathSound);
+        }
+        
+    }
+
     //audio for gumbo and his fart rocket
     private void PlayGumboFart()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (state == State.Alive)
         {
-            
-            //start fart sound for gumbo
-            if (playFart == true && toggleFartSound == true)
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
-                audioSource.Play();
-                toggleFartSound = false;
+
+                //start fart sound for gumbo
+                if (playFart == true && toggleFartSound == true)
+                {
+                    audioSource.PlayOneShot(Farts);
+                    Gas.Play();
+                    toggleFartSound = false;
+                }
+
             }
-
+            //stop fart sound for gumbo
+            if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                audioSource.Stop();
+                Gas.Stop();
+                toggleFartSound = true;
+            }
         }
-        //stop fart sound for gumbo
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
-        {
-            audioSource.Stop();
-            toggleFartSound = true;
-        }
+        
     }
-
+    // functions that are invoked for level competion and death
+    //they say they are not referenced but they are used through invoke within OnCollisonEnter so don't touch 
     private void LoadNextLevel()
     {
         SceneManager.LoadScene(nextLevel);
@@ -103,7 +127,7 @@ public class Fart : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
-
+    // Defines what happens when character runs into stuff
     void OnCollisionEnter(Collision collision)
     {
         if (state != State.Alive)
@@ -113,17 +137,21 @@ public class Fart : MonoBehaviour
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
+                
                 break;
             case "Finish":
                 state = State.NextLevel;
+                SuccessParticle.Play();
                 Invoke ("LoadNextLevel", nextLevelDelay);
                 break;
             case "Food":
-                print("Food");
+                
                 break;
             default:
+                
                 state = State.Dying;
+                DeathAudio();
+                DeathParticle.Play();
                 Invoke("Death", deathDelay);
                 break;
 
---
+++ Spaceman-Gumbo/Assets/Victory.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    AudioSource audioSource;
+    enum State { Alive, Dying, NextLevel };
+    State state = State.Alive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive)
+        {
+            return;
+        }
+        switch (collision.gameObject.tag)
+        {
+            
+            default:
+                state = State.NextLevel;
+                audioSource.Play();
+                
+                break;
+
+        }
+    }
+}
